50cd68b3516040a88c05f6d414e655885c1dcb1c
yubinary <yubinheo356@gmail.com>
2020-12-28 16:43:51 +0900

    Initialized React

---
+++ itunes/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainContent from "./pages/MainContent.js";
+import SideBar from "./pages/SideBar.js";
+
+function App() {
+  return (
+    <div className="App">
+      <MainContent />
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
